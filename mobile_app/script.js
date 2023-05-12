cdc5356bc79f1f019c3cdaf71986107730e9f4e7
XeniyaZ <kseniya10081989@gmail.com>
2023-05-12 11:14:11 -0500

    initialize new app for firebase learning

---
+++ mobile_app/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('add-button')
+const input = document.getElementById('input-field')
+
+
+
+btn.addEventListener('click', function(){
+    let inputValue = input.value
+console.log(inputValue)
+})
